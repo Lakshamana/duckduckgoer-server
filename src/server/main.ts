1629b5a19570402a578128c5ff486acc67055174
Lakhshamana <lakshamana@protonmail.com>
2024-08-15 23:30:13 -0300

    chore: added helmet, cors configuration

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
+import { ValidationPipe } from '@nestjs/common'
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  }
+
+  app.enableCors(corsOptions)
+
+  app.use(helmet())
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  )
+
   await app.listen(process.env.PORT || 3333)
 }
+
 bootstrap()
